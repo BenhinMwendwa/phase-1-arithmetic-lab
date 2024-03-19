add9bc82362996aeebcaf0da7a4e229a5c2fd745
Benhin <benhinmwendwa7@gmail.com>
2024-03-19 15:44:54 +0300

    maths

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
-// Write your code here
+let num1=31;
+let num2=2;
+let multiply = (num1*num2);
+console.log(multiply);
+let random = Math.floor(Math.random() * 6 + 1)
+let num3=40
+let num4=6
+const mod=num3%num4
+const max=Math.max(12,3,20)
